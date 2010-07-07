dfd098aa95f803b2692dd02a770e0d58fd11774d
Andy Rossmeissl <andy@rossmeissl.net>
2010-07-07 10:56:03 -0400

    Force Rails to look at the gem's public dir by adding middleware

--- lib/brighter_planet_layout/railtie.rb
+++ lib/brighter_planet_layout/railtie.rb
@@ -1,8 +1,8 @@
 module BrighterPlanetLayout
   class Railtie < Rails::Railtie
+    config.app_middleware.use '::ActionDispatch::Static', File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'public'))    
     initializer 'brighter_planet_layout.add_paths' do |app|
       app.paths.app.views << File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'app', 'views'))
-      app.paths.public.stylesheets << File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'public', 'stylesheets'))
       ApplicationController.helper BrighterPlanetHelper
     end
   end
