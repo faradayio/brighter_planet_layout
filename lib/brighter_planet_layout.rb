55a129a1235faf6e1c167dd19d1ea5e1513df35e
Andy Rossmeissl <andy@rossmeissl.net>
2010-07-07 00:23:02 -0400

    the helpers path didn't work

--- lib/brighter_planet_layout.rb
+++ lib/brighter_planet_layout.rb
@@ -1,1 +1,2 @@
 require 'brighter_planet_layout/railtie' if defined?(Rails::Railtie)
+require File.join(File.dirname(__FILE__),'..', 'app', 'helpers', 'brighter_planet_helper.rb')
--- lib/brighter_planet_layout/railtie.rb
+++ lib/brighter_planet_layout/railtie.rb
@@ -2,8 +2,8 @@ module BrighterPlanetLayout
   class Railtie < Rails::Railtie
     initializer 'brighter_planet_layout.add_paths' do |app|
       app.paths.app.views << File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'app', 'views'))
-      app.paths.app.helpers << File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'app', 'helpers'))
       app.paths.public.stylesheets << File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'public', 'stylesheets'))
+      ApplicationController.helper BrighterPlanetHelper
     end
   end
 end
