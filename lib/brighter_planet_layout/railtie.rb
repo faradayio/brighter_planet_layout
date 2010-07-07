1e8a171a22501641c5b7c37ccfc10010597159c2
Andy Rossmeissl <andy@rossmeissl.net>
2010-07-07 14:30:31 -0400

    Aha: config.to_prepare runs with each request

--- lib/brighter_planet_layout/railtie.rb
+++ lib/brighter_planet_layout/railtie.rb
@@ -3,7 +3,10 @@ module BrighterPlanetLayout
     config.app_middleware.use '::ActionDispatch::Static', File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'public'))    
     initializer 'brighter_planet_layout.add_paths' do |app|
       app.paths.app.views << File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'app', 'views'))
+    end
+    config.to_prepare do
       ApplicationController.helper BrighterPlanetHelper
+      ApplicationController.layout 'brighter_planet'
     end
   end
 end
