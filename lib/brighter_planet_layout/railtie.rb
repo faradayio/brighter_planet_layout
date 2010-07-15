17bca0c71921dfac567c7978659ddc1eb6b8bbee
Andy Rossmeissl <andy@rossmeissl.net>
2010-07-15 15:38:36 -0400

    Use the old method on dev and Seamus's method on production

--- lib/brighter_planet_layout/railtie.rb
+++ lib/brighter_planet_layout/railtie.rb
@@ -1,11 +1,12 @@
 module BrighterPlanetLayout
   class Railtie < Rails::Railtie
+    config.app_middleware.use '::ActionDispatch::Static', BrighterPlanetLayout.public_path unless ::Rails.env.production?
     initializer 'brighter_planet_layout.add_paths' do |app|
       app.paths.app.views.push BrighterPlanetLayout.view_path
     end
-    initializer 'brighter_planet_layout.copy_static_files_to_web_server_document_root' do |app|
+    initializer 'brighter_planet_layout.copy_static_files_to_web_server_document_root' do
       BrighterPlanetLayout.copy_static_files_to_web_server_document_root
-    end
+    end if ::Rails.env.production?
     config.to_prepare do
       require BrighterPlanetLayout.helper_file
       ApplicationController.helper BrighterPlanetHelper
