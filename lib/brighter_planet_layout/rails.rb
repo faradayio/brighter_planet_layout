e6e4df7b63ebd4d1a5d08bfe7ec498a1a9f3f3cc
Seamus Abshere <seamus@abshere.net>
2010-07-28 23:56:51 -0500

    for rails 2 apps that might still use other layouts, don't force layout 'brighter_planet'

--- lib/brighter_planet_layout/rails.rb
+++ lib/brighter_planet_layout/rails.rb
@@ -3,7 +3,8 @@ if defined? ::Rails and ::Rails::VERSION::MAJOR == 2
   require ::BrighterPlanetLayout.helper_file
   ::Rails.configuration.to_prepare do
     ::ApplicationController.helper ::BrighterPlanetHelper
-    ::ApplicationController.layout 'brighter_planet'
+    # sabshere 7/29/10 this makes it impossible to apply selectively
+    # ::ApplicationController.layout 'brighter_planet'
     # sabshere 7/29/10 the view path appears to be magically appended
     # ::ApplicationController.append_view_path ::BrighterPlanetLayout.view_path
   end
