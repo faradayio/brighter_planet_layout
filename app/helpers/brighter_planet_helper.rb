f6c35a760c90f0c029955dd5e597bc04ae8a693b
Andy Rossmeissl <andy@rossmeissl.net>
2010-07-29 12:22:32 -0500

    Provide link_to_homesite so that navbars go relative when on bp.com

--- app/helpers/brighter_planet_helper.rb
+++ app/helpers/brighter_planet_helper.rb
@@ -7,7 +7,13 @@ module BrighterPlanetHelper
       nil
     end
   end
+
   def syntax(code, lang = 'ruby')
     ::Uv.parse(code, 'xhtml', lang, false, :idle).html_safe
+  end
+  
+  def link_to_homesite(text, path = '')
+    path.insert 0, 'http://brighterplanet.com/' unless ::BrighterPlanetLayout.application_name == 'Brighter Planet'
+    link_to text, path
   end
 end
