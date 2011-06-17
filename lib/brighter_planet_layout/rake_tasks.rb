6a75db1a92672353d268d9aebe0fdd6d408dcec2
Derek Kastner <dkastner@gmail.com>
2011-06-17 15:03:12 -0400

    Added description to rake task

--- lib/brighter_planet_layout/rake_tasks.rb
+++ lib/brighter_planet_layout/rake_tasks.rb
@@ -1,4 +1,5 @@
 namespace :brighter_planet_layout do
+  desc 'Copy static assets from BPL gem into rails app (Rails 2)'
   task :copy_static_files => :environment do
     ::BrighterPlanet.layout.copy_static_files_to_web_server_document_root
   end
